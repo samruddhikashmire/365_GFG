bbbe761c09e69ac3fb2ab771584d961320be40b6
Samruddhi Kashmire <104366974+samruddhikashmire@users.noreply.github.com>
2024-04-13 23:31:16 +0530

    Create Day1.cpp
    Reverse Bits

---
+++ Day1.cpp
@@ -0,0 +1,36 @@
+// Given a number x, reverse its binary form and return the answer in decimal.
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        
+         long long ans = 0;
+        for(int i = 0;i < 31; i++) {
+            ans |= (x&1);
+            ans <<= 1;
+            x >>= 1;
+        }
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
